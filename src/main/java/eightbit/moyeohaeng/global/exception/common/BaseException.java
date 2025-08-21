8d00199921e61af861313f1bfc90f92c09fd7739
Cori1304-Seong <andyjm7739@gmail.com>
2025-08-21 14:44:28 +0900

    feat: reponse에 주요 데이터 노출 방지(#1)

--- src/main/java/eightbit/moyeohaeng/global/exception/common/BaseException.java
+++ src/main/java/eightbit/moyeohaeng/global/exception/common/BaseException.java
@@ -6,14 +6,24 @@ import lombok.Getter;
 public abstract class BaseException extends RuntimeException {
 
 	private final ErrorCode errorCode;
+	private final transient Object[] args;
 
 	public BaseException(ErrorCode errorCode) {
 		super(errorCode.getMessage());
 		this.errorCode = errorCode;
+		this.args = new Object[]{};
 	}
 
 	public BaseException(ErrorCode errorCode, Object... args) {
-		super(errorCode.getMessage(args));
+		super(errorCode.getMessage());
 		this.errorCode = errorCode;
+		this.args = args;
+	}
+
+	public String getLogMessage() {
+		if (args == null || args.length == 0) {
+			return errorCode.getMessage();
+		}
+		return errorCode.getMessage(args);
 	}
 }
