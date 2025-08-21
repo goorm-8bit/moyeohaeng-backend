6dd19228a4774ade7c54474cfff6e50d81b7ef5c
Cori1304-Seong <andyjm7739@gmail.com>
2025-08-21 13:43:05 +0900

    feat: GlobalLogger 추가 (#1)
    - 로그 시스템 표준화와 효율적인 로그 관리를 하기 위해서

---
+++ src/main/java/eightbit/moyeohaeng/global/utils/GlobalLogger.java
@@ -0,0 +1,29 @@
+package eightbit.moyeohaeng.global.utils;
+
+import java.util.stream.IntStream;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class GlobalLogger {
+	private static String BLOCK = "{} ";
+
+	private static String format(int length) {
+		StringBuilder format = new StringBuilder();
+		IntStream.range(0, length).forEach(i -> format.append(BLOCK));
+
+		return format.toString();
+	}
+
+	public static void warn(Object... args) {
+		log.warn(format(args.length), args);
+	}
+
+	public static void info(Object... args) {
+		log.info(format(args.length), args);
+	}
+
+	public static void error(Object... args) {
+		log.error(format(args.length), args);
+	}
+}
