6c91d791f0095d18a1effa40b3d457c9b0740f20
조윤성 <yunsung1206@gmail.com>
2025-08-21 10:02:58 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/eightbit/moyeohaeng/MoyeohaengApplication.java
@@ -0,0 +1,13 @@
+package eightbit.moyeohaeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoyeohaengApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MoyeohaengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eightbit/moyeohaeng/MoyeohaengApplicationTests.java
@@ -0,0 +1,13 @@
+package eightbit.moyeohaeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoyeohaengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
