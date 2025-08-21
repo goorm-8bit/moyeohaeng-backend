e08d9609b5e64882980ef61590b79c916c4797cd
Cori1304-Seong <andyjm7739@gmail.com>
2025-08-21 13:53:55 +0900

    feat: GlobalException 로직 추가 (#1)

---
+++ src/main/java/eightbit/moyeohaeng/global/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,65 @@
+package eightbit.moyeohaeng.global.exception.handler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MissingRequestHeaderException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import eightbit.moyeohaeng.global.exception.common.BaseException;
+import eightbit.moyeohaeng.global.exception.common.ErrorCode;
+import eightbit.moyeohaeng.global.exception.common.ErrorResponse;
+import eightbit.moyeohaeng.global.utils.GlobalLogger;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(BaseException.class)
+	public ResponseEntity<ErrorResponse> handleBaseException(BaseException e) {
+		return getErrorResponse(e, e.getErrorCode());
+	}
+
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
+		return getErrorResponse(e, GlobalErrorCode.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorResponse> handleException(Exception e) {
+		return getErrorResponse(e, GlobalErrorCode.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler(MissingRequestHeaderException.class)
+	public ResponseEntity<ErrorResponse> handleMissingRequestHeaderException(MissingRequestHeaderException e) {
+		return getErrorResponse(e, GlobalErrorCode.MISSING_HEADER);
+	}
+
+	@ExceptionHandler(NoResourceFoundException.class)
+	public ResponseEntity<ErrorResponse> handleNoResourceException(NoResourceFoundException e) {
+		return getErrorResponse(e, GlobalErrorCode.RESOURCE_NOT_FOUND);
+	}
+
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	public ResponseEntity<ErrorResponse>
+	handleMethodNotAllowedException(HttpRequestMethodNotSupportedException e) {
+		return getErrorResponse(e, GlobalErrorCode.METHOD_NOT_ALLOWED);
+	}
+
+	private static ResponseEntity<ErrorResponse> getErrorResponse(Exception e, GlobalErrorCode errorCode) {
+		GlobalLogger.error(e.toString());
+
+		return ResponseEntity
+			.status(errorCode.getStatus())
+			.body(ErrorResponse.of(errorCode));
+	}
+
+	private static ResponseEntity<ErrorResponse> getErrorResponse(Exception e, ErrorCode errorCode) {
+		GlobalLogger.error(e.toString());
+
+		return ResponseEntity
+			.status(errorCode.getStatus())
+			.body(ErrorResponse.of(errorCode, e.getMessage()));
+	}
+
+}
