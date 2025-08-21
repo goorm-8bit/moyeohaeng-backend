6350f5b1353e48ffeb682748a6161695369c3eef
jh <biruk23@naver.com>
2025-08-21 23:19:40 +0900

    feat: @EnableJpaAuditing 적용 (#2)

--- src/main/java/eightbit/moyeohaeng/MoyeohaengApplication.java
+++ src/main/java/eightbit/moyeohaeng/MoyeohaengApplication.java
@@ -2,10 +2,12 @@ package eightbit.moyeohaeng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MoyeohaengApplication {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(MoyeohaengApplication.class, args);
 	}
