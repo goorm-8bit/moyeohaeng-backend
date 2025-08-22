8dccb6d71cec99a4c370cf68a75e58b90c1f1ce0
조윤성 <yunsung1206@gmail.com>
2025-08-22 17:40:07 +0900

    test: JsonUtils 추가 (#10)

--- src/test/java/eightbit/moyeohaeng/support/annotation/IntegrationTest.java
+++ src/test/java/eightbit/moyeohaeng/support/annotation/IntegrationTest.java
@@ -8,15 +8,18 @@ import java.lang.annotation.Target;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.context.ImportTestcontainers;
+import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Transactional;
 
 import eightbit.moyeohaeng.support.container.TestContainers;
+import eightbit.moyeohaeng.support.util.JsonUtils;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @ImportTestcontainers(TestContainers.class)
+@Import({JsonUtils.class})
 @Transactional
 public @interface IntegrationTest {
 }
