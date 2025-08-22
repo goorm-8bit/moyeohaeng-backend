a8adba94652fa3819fb39b498eacb12089a3d08a
Cori1304-Seong <andyjm7739@gmail.com>
2025-08-22 13:57:12 +0900

    feat: BaseException에 NPE 방지 추가 (#1)

--- src/main/java/eightbit/moyeohaeng/global/exception/common/BaseException.java
+++ src/main/java/eightbit/moyeohaeng/global/exception/common/BaseException.java
@@ -1,23 +1,28 @@
 package eightbit.moyeohaeng.global.exception.common;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 import lombok.Getter;
 
 @Getter
 public abstract class BaseException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	private static final Object[] EMPTY_ARGS = {};
 
 	private final ErrorCode errorCode;
 	private final transient Object[] args;
 
 	public BaseException(ErrorCode errorCode) {
 		super(errorCode.getMessage());
-		this.errorCode = errorCode;
-		this.args = new Object[]{};
+		this.errorCode = Objects.requireNonNull(errorCode, "errorCode must not be null");
+		this.args = EMPTY_ARGS;
 	}
 
 	public BaseException(ErrorCode errorCode, Object... args) {
 		super(errorCode.getMessage());
-		this.errorCode = errorCode;
-		this.args = args;
+		this.errorCode = Objects.requireNonNull(errorCode, "errorCode must not be null");
+		this.args = (args == null ? EMPTY_ARGS : Arrays.copyOf(args, args.length));
 	}
 
 	public String getLogMessage() {
