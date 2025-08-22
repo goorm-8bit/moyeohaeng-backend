c77742c53e3a38af11fa23bfe7a12c64ffd5f179
조윤성 <yunsung1206@gmail.com>
2025-08-22 16:46:47 +0900

    test: RANDOM_PORT 제거 및 MockMvc 사용 (#10)

--- src/test/java/eightbit/moyeohaeng/support/annotation/IntegrationTest.java
+++ src/test/java/eightbit/moyeohaeng/support/annotation/IntegrationTest.java
@@ -1,15 +1,20 @@
 package eightbit.moyeohaeng.support.annotation;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.context.ImportTestcontainers;
 import org.springframework.transaction.annotation.Transactional;
 
 import eightbit.moyeohaeng.support.container.TestContainers;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@AutoConfigureMockMvc
+@Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @ImportTestcontainers(TestContainers.class)
 @Transactional
