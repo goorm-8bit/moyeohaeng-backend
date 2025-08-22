9f9dfda7972daa412fd036adb49d547c5d133a36
조윤성 <yunsung1206@gmail.com>
2025-08-22 18:33:33 +0900

    test: 중복 어노테이션 제거 (#10)

--- src/test/java/eightbit/moyeohaeng/support/util/JsonUtils.java
+++ src/test/java/eightbit/moyeohaeng/support/util/JsonUtils.java
@@ -4,13 +4,11 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.TestComponent;
 import org.springframework.test.web.servlet.MvcResult;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@TestComponent
 public class JsonUtils {
 
 	@Autowired
