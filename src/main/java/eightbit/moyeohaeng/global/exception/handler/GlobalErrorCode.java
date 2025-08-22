f0d0b16b3c5994445d6d46cfe0981bdcc1096121
Cori1304-Seong <andyjm7739@gmail.com>
2025-08-22 14:12:30 +0900

    refactor: http로 그룹화 (#1)

--- src/main/java/eightbit/moyeohaeng/global/exception/handler/GlobalErrorCode.java
+++ src/main/java/eightbit/moyeohaeng/global/exception/handler/GlobalErrorCode.java
@@ -7,18 +7,20 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public enum GlobalErrorCode implements ErrorCode {
-	RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "리소스를 찾을 수 없습니다.", "G4001"),
-	METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "지원하지 않는 메소드입니다.", "G4002"),
-	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.", "G5001"),
-	MISSING_HEADER(HttpStatus.BAD_REQUEST, "요청에 필요한 헤더가 존재하지 않습니다.", "G4003"),
-	FORBIDDEN(HttpStatus.FORBIDDEN, "접근이 금지되었습니다.", "G4004"),
-	INVALID_PATH_VARIABLE_FORMAT(HttpStatus.BAD_REQUEST, "요청 경로 파라미터의 형식이 올바르지 않습니다.", "G4005"),
-	INVALID_INPUT(HttpStatus.BAD_REQUEST, "입력값이 올바르지 않습니다.", "G4006");
 
-	public static final String PREFIX = "[GLOBAL ERROR] ";
+	// 4xx
+	RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "리소스를 찾을 수 없습니다.", "G1001"),
+	METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "지원하지 않는 메소드입니다.", "G1002"),
+	MISSING_HEADER(HttpStatus.BAD_REQUEST, "요청에 필요한 헤더가 존재하지 않습니다.", "G1003"),
+	FORBIDDEN(HttpStatus.FORBIDDEN, "접근이 금지되었습니다.", "G1004"),
+	INVALID_PATH_VARIABLE_FORMAT(HttpStatus.BAD_REQUEST, "요청 경로 파라미터의 형식이 올바르지 않습니다.", "G1005"),
+	INVALID_INPUT(HttpStatus.BAD_REQUEST, "입력값이 올바르지 않습니다.", "G1006"),
+
+	// 5xx
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.", "G1007");
 
 	private final HttpStatus status;
-	private final String rawMessage;
+	private final String message;
 	private final String code;
 
 	@Override
@@ -28,9 +30,9 @@ public enum GlobalErrorCode implements ErrorCode {
 
 	@Override
 	public String getMessage() {
-		return PREFIX + rawMessage;
+		return message;
 	}
-	
+
 	@Override
 	public String getCode() {
 		return code;
