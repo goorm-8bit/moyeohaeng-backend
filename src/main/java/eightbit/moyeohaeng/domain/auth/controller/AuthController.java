cf736867657873cede7854a78dafab86a328de4f
Cori1304-Seong <andyjm7739@gmail.com>
2025-08-24 01:24:26 +0900

    feat: AuthController 추가 (#9)

---
+++ src/main/java/eightbit/moyeohaeng/domain/auth/controller/AuthController.java
@@ -0,0 +1,27 @@
+package eightbit.moyeohaeng.domain.auth.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import eightbit.moyeohaeng.domain.auth.dto.SignUpRequest;
+import eightbit.moyeohaeng.domain.auth.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+	private final AuthService authService;
+
+	@PostMapping("/register")
+	public ResponseEntity<Void> signUp(@Valid @RequestBody SignUpRequest signUpRequest) {
+		authService.signUp(signUpRequest);
+		return ResponseEntity.ok().build();
+	}
+
+}
