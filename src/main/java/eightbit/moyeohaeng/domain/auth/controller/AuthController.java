94e5badd04164423bdbdcf161117fc8bb708f924
Cori1304-Seong <andyjm7739@gmail.com>
2025-08-25 01:03:46 +0900

    refactor: SignUpRequest 파일 이동 (#13)

--- src/main/java/eightbit/moyeohaeng/domain/auth/controller/AuthController.java
+++ src/main/java/eightbit/moyeohaeng/domain/auth/controller/AuthController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import eightbit.moyeohaeng.domain.auth.dto.SignUpRequest;
+import eightbit.moyeohaeng.domain.auth.dto.request.SignUpRequest;
 import eightbit.moyeohaeng.domain.auth.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
