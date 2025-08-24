5a11e531237ca8d503b256382bacdd27d27d0329
Cori1304-Seong <andyjm7739@gmail.com>
2025-08-25 00:38:25 +0900

    feat: CORS 설정 추가 (#9)

--- src/main/java/eightbit/moyeohaeng/global/config/SecurityConfig.java
+++ src/main/java/eightbit/moyeohaeng/global/config/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.web.cors.CorsConfigurationSource;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -18,8 +18,9 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	public SecurityFilterChain filterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
 		return http
+			.cors(cors -> cors.configurationSource(corsConfigurationSource))
 			.csrf(AbstractHttpConfigurer::disable)
 			.httpBasic(AbstractHttpConfigurer::disable)
 			.formLogin(AbstractHttpConfigurer::disable)
