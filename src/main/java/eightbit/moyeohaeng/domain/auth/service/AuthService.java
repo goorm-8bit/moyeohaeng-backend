736c9cc13704eab65c1ebc3361c757afd0ea012f
Cori1304-Seong <andyjm7739@gmail.com>
2025-08-25 01:28:34 +0900

    feat: 회원 가입 기능 추가 (#13)
    1. AuthService에 login() 추가
    2. AuthController @PostMapping("/login") 추가

--- src/main/java/eightbit/moyeohaeng/domain/auth/service/AuthService.java
+++ src/main/java/eightbit/moyeohaeng/domain/auth/service/AuthService.java
@@ -4,11 +4,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import eightbit.moyeohaeng.domain.auth.dto.SignUpRequest;
+import eightbit.moyeohaeng.domain.auth.dto.request.LoginRequest;
+import eightbit.moyeohaeng.domain.auth.dto.request.SignUpRequest;
+import eightbit.moyeohaeng.domain.auth.dto.response.TokenResponse;
 import eightbit.moyeohaeng.domain.auth.exception.AuthErrorCode;
 import eightbit.moyeohaeng.domain.auth.exception.AuthException;
 import eightbit.moyeohaeng.domain.member.entity.member.Member;
 import eightbit.moyeohaeng.domain.member.repository.MemberRepository;
+import eightbit.moyeohaeng.global.domain.auth.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -18,6 +21,7 @@ public class AuthService {
 
 	private final MemberRepository memberRepository;
 	private final PasswordEncoder passwordEncoder;
+	private final JwtTokenProvider jwtTokenProvider;
 
 	@Transactional
 	public void signUp(SignUpRequest signUpRequest) {
@@ -32,6 +36,20 @@ public class AuthService {
 			.build();
 
 		memberRepository.save(member);
+	}
+
+	public TokenResponse login(LoginRequest loginRequest) {
+		Member member = memberRepository.findByEmail(loginRequest.email())
+			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 이메일입니다."));
+
+		if (!passwordEncoder.matches(loginRequest.password(), member.getPassword())) {
+			throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+		}
+
+		String accessToken = jwtTokenProvider.createAccessToken(String.valueOf(member.getId()));
+		String refreshToken = jwtTokenProvider.createRefreshToken(String.valueOf(member.getId()));
+
+		return new TokenResponse(accessToken, refreshToken);
 	}
 
 }
