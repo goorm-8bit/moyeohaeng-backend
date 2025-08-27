e646078f3e7665f9dd5c66e02fadf1fdd248bca7
jh <biruk23@naver.com>
2025-08-27 23:31:37 +0900

    remove: Integer 타입으로 변경하면서 @Enumerated 삭제 (#46)

--- src/main/java/eightbit/moyeohaeng/domain/project/entity/TimeBlock.java
+++ src/main/java/eightbit/moyeohaeng/domain/project/entity/TimeBlock.java
@@ -9,8 +9,6 @@ import org.hibernate.annotations.SQLRestriction;
 import eightbit.moyeohaeng.global.domain.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -40,7 +38,6 @@ public class TimeBlock extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Enumerated(EnumType.STRING)
 	@Column(nullable = false, length = 10)
 	private Integer day;
 
