0e2ae82afa842d3bb8ee77be00c775d0f41916d8
jh <biruk23@naver.com>
2025-08-27 14:21:08 +0900

    feat: PlaceGroup Entity 추가 (#46)

---
+++ src/main/java/eightbit/moyeohaeng/domain/project/entity/PlaceGroup.java
@@ -0,0 +1,38 @@
+package eightbit.moyeohaeng.domain.project.entity;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import eightbit.moyeohaeng.global.domain.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "place_groups")
+@SQLDelete(sql = "UPDATE place_groups SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
+@SQLRestriction("deleted_at IS NULL")
+public class PlaceGroup extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, length = 50)
+	private String name;
+
+	@Column(length = 20)
+	private String color;
+}
