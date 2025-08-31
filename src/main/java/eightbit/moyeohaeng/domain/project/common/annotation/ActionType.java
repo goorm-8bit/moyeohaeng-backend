4b60c3d9bcb0bce5cc4e624aa3d1a82aba804588
조윤성 <yunsung1206@gmail.com>
2025-08-31 10:57:33 +0900

    feat: ProjectEvent 어노테이션 추가 (#63)

---
+++ src/main/java/eightbit/moyeohaeng/domain/project/common/annotation/ActionType.java
@@ -0,0 +1,7 @@
+package eightbit.moyeohaeng.domain.project.common.annotation;
+
+public enum ActionType {
+	CREATED,
+	UPDATED,
+	DELETED
+}
---
+++ src/main/java/eightbit/moyeohaeng/domain/project/common/annotation/EventType.java
@@ -0,0 +1,13 @@
+package eightbit.moyeohaeng.domain.project.common.annotation;
+
+public enum EventType {
+	PIN,
+	PLACE_BLOCK,
+	PLACE_BLOCK_COMMENT,
+	PLACE_BLOCK_LIKE,
+	PLACE_GROUP,
+	PROJECT,
+	TIME_BLOCK,
+	SCRAP_BOOK,
+	MEMBER_PRESENCE
+}
