1ed09e2cc5e0fb8537d572932d693a4996f0123b
currysoda <currysoda1598@gmail.com>
2025-09-02 15:35:17 +0900

    feat: wip (#33)

--- src/main/java/eightbit/moyeohaeng/domain/team/controller/TeamController.java
+++ src/main/java/eightbit/moyeohaeng/domain/team/controller/TeamController.java
@@ -2,6 +2,7 @@ package eightbit.moyeohaeng.domain.team.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/teams")
 @Slf4j
 public class TeamController implements TeamApi{
-
-
+	
+	@Override
+	public ResponseEntity inviteMember() {
+		return null;
+	}
+	
+	@Override
+	public ResponseEntity creationTeam() {
+		return null;
+	}
+	
+	@Override
+	public ResponseEntity removeMember() {
+		return null;
+	}
+	
+	@Override
+	public ResponseEntity updateMemberRole() {
+		return null;
+	}
 }
--- src/main/java/eightbit/moyeohaeng/domain/team/repository/TeamRepository.java
+++ src/main/java/eightbit/moyeohaeng/domain/team/repository/TeamRepository.java
@@ -1,10 +1,10 @@
 package eightbit.moyeohaeng.domain.team.repository;
 
+import eightbit.moyeohaeng.domain.team.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import eightbit.moyeohaeng.domain.team.entity.Team;
-
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
+
 }
