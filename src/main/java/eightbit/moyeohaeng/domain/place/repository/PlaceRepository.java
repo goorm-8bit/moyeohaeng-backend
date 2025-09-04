7301e1ab3015573599fd9d3476845212d087f6fb
jh <biruk23@naver.com>
2025-09-05 01:44:23 +0900

    feat: 위도·경도로 장소 조회 메서드 추가 (#18)

--- src/main/java/eightbit/moyeohaeng/domain/place/repository/PlaceRepository.java
+++ src/main/java/eightbit/moyeohaeng/domain/place/repository/PlaceRepository.java
@@ -1,5 +1,7 @@
 package eightbit.moyeohaeng.domain.place.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import eightbit.moyeohaeng.domain.place.entity.Place;
 
 @Repository
 public interface PlaceRepository extends JpaRepository<Place, Long> {
+	Optional<Place> findByLatitudeAndLongitude(Double latitude, Double longitude);
 }
