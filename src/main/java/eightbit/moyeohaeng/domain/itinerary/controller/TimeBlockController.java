0dcc1ae5eec3f655ed4fb29b4427260423e4a22c
조윤성 <yunsung1206@gmail.com>
2025-09-04 21:14:35 +0900

    feat: 시간 블록 수정 API 구현 (#68)

--- src/main/java/eightbit/moyeohaeng/domain/itinerary/controller/TimeBlockController.java
+++ src/main/java/eightbit/moyeohaeng/domain/itinerary/controller/TimeBlockController.java
@@ -3,6 +3,7 @@ package eightbit.moyeohaeng.domain.itinerary.controller;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import eightbit.moyeohaeng.domain.itinerary.common.success.TimeBlockSuccessCode;
 import eightbit.moyeohaeng.domain.itinerary.controller.swagger.TimeBlockApi;
 import eightbit.moyeohaeng.domain.itinerary.dto.request.TimeBlockCreateRequest;
+import eightbit.moyeohaeng.domain.itinerary.dto.request.TimeBlockUpdateRequest;
 import eightbit.moyeohaeng.domain.itinerary.dto.response.TimeBlockResponse;
 import eightbit.moyeohaeng.domain.itinerary.service.TimeBlockService;
 import eightbit.moyeohaeng.global.security.CustomUserDetails;
@@ -33,5 +35,17 @@ public class TimeBlockController implements TimeBlockApi {
 	) {
 		TimeBlockResponse response = timeBlockService.create(projectId, request);
 		return SuccessResponse.of(TimeBlockSuccessCode.CREATE_TIME_BLOCK, response);
+	}
+
+	@Override
+	@PutMapping("/{timeBlockId}")
+	public SuccessResponse<TimeBlockResponse> update(
+		@PathVariable Long projectId,
+		@PathVariable Long timeBlockId,
+		@Valid @RequestBody TimeBlockUpdateRequest request,
+		@AuthenticationPrincipal CustomUserDetails currentUser
+	) {
+		TimeBlockResponse response = timeBlockService.update(projectId, timeBlockId, request);
+		return SuccessResponse.of(TimeBlockSuccessCode.UPDATE_TIME_BLOCK, response);
 	}
 }
