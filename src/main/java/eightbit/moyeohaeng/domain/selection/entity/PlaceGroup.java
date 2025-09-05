15a9f56e1d8f1be2ceac9a9d8861f26e9e87d808
조윤성 <yunsung1206@gmail.com>
2025-09-05 13:44:35 +0900

    refactor: 이름에 20자 제한 추가 (#74)

--- src/main/java/eightbit/moyeohaeng/domain/selection/entity/PlaceGroup.java
+++ src/main/java/eightbit/moyeohaeng/domain/selection/entity/PlaceGroup.java
@@ -35,7 +35,7 @@ public class PlaceGroup extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(name = "name", nullable = false)
+	@Column(name = "name", nullable = false, length = 20)
 	private String name;
 
 	@Column(name = "color", nullable = false, length = 7)
