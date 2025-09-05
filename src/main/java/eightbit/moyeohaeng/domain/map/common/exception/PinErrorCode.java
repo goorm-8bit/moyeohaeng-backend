a5b2eb167e0f25cab725997658c91e1cd58ccf36
jh <biruk23@naver.com>
2025-09-05 12:48:07 +0900

    refactor: 에러 코드 충돌 방지를 위해 핀 에러 코드 PIN으로 변경 (#18)

--- src/main/java/eightbit/moyeohaeng/domain/map/common/exception/PinErrorCode.java
+++ src/main/java/eightbit/moyeohaeng/domain/map/common/exception/PinErrorCode.java
@@ -10,8 +10,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum PinErrorCode implements ErrorCode {
 
-	PIN_NOT_FOUND(HttpStatus.NOT_FOUND, "핀을 찾을 수 없습니다.", "P1001"),
-	DUPLICATE_PIN(HttpStatus.CONFLICT, "이미 존재하는 핀입니다.", "P1002");
+	PIN_NOT_FOUND(HttpStatus.NOT_FOUND, "핀을 찾을 수 없습니다.", "PIN1001"),
+	DUPLICATE_PIN(HttpStatus.CONFLICT, "이미 존재하는 핀입니다.", "PIN1002");
 
 	private final HttpStatus status;
 	private final String message;
