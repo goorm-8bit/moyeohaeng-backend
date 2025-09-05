90f1ec730a80c981860610d3d302b9a0ba4be890
조윤성 <yunsung1206@gmail.com>
2025-09-05 22:15:07 +0900

    feat: 하트비트를 위한 스케줄링 기능 활성화 (#63)

--- src/main/java/eightbit/moyeohaeng/MoyeohaengApplication.java
+++ src/main/java/eightbit/moyeohaeng/MoyeohaengApplication.java
@@ -4,12 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 public class MoyeohaengApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoyeohaengApplication.class, args);
 	}
-
 }
