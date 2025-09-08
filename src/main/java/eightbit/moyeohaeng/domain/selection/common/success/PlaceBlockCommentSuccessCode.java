b9c5773468155856cacccebefb15864e45320612
jh <biruk23@naver.com>
2025-09-08 15:04:46 +0900

    feat: PlaceBlockCommentSuccessCode에 DELETE_COMMENT 추가 (#16)

--- src/main/java/eightbit/moyeohaeng/domain/selection/common/success/PlaceBlockCommentSuccessCode.java
+++ src/main/java/eightbit/moyeohaeng/domain/selection/common/success/PlaceBlockCommentSuccessCode.java
@@ -12,6 +12,7 @@ public enum PlaceBlockCommentSuccessCode implements SuccessCode {
 
 	CREATE_COMMENT(HttpStatus.CREATED, "댓글 생성에 성공했습니다."),
 	UPDATE_COMMENT(HttpStatus.OK, "댓글 수정에 성공했습니다."),
+	DELETE_COMMENT(HttpStatus.OK, "댓글 삭제에 성공했습니다."),
 	GET_COMMENT_LIST(HttpStatus.OK, "댓글 목록을 조회했습니다."),
 	GET_COMMENT_SUMMARY(HttpStatus.OK, "댓글 요약 정보를 조회했습니다.");
 
