4b4c6427ada27ee8e1f0b6e92cd8c080da79ae74
Cori1304-Seong <andyjm7739@gmail.com>
2025-09-08 14:16:22 +0900

    feat: deletedAt 검사 로직 추가

--- src/main/java/eightbit/moyeohaeng/domain/project/repository/ProjectRepository.java
+++ src/main/java/eightbit/moyeohaeng/domain/project/repository/ProjectRepository.java
@@ -14,24 +14,35 @@ import eightbit.moyeohaeng.domain.project.entity.Project;
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
-	Optional<Project> findByExternalId(String externalId);
+    Optional<Project> findByExternalIdAndDeletedAtIsNull(String externalId);
 
-	@Query("SELECT p FROM Project p WHERE p.team.id = :teamId")
-	List<Project> findByTeamId(@Param("teamId") Long teamId, Sort sort);
+    @Query("SELECT p FROM Project p " +
+        "JOIN p.team t " +
+        "WHERE t.id = :teamId " +
+        "AND p.deletedAt IS NULL " +
+        "AND t.deletedAt IS NULL")
+    List<Project> findActiveByTeamId(@Param("teamId") Long teamId, Sort sort);
 
-	@Query("SELECT DISTINCT p FROM Project p " +
-		"JOIN p.team t " +
-		"JOIN TeamMember tm ON tm.team = t " +
-		"WHERE tm.member.id = :memberId")
-	List<Project> findByMemberId(@Param("memberId") Long memberId, Sort sort);
+    @Query("SELECT DISTINCT p FROM Project p " +
+        "JOIN p.team t " +
+        "JOIN TeamMember tm ON tm.team = t AND tm.deletedAt IS NULL " +
+        "WHERE tm.member.id = :memberId " +
+        "AND p.deletedAt IS NULL " +
+        "AND t.deletedAt IS NULL")
+    List<Project> findByMemberId(@Param("memberId") Long memberId, Sort sort);
 
-	@Query("SELECT p FROM Project p " +
-		"WHERE p.id = :projectId " +
-		"AND (p.creator.id = :memberId " +
-		"OR EXISTS (" +
-		"    SELECT tm FROM TeamMember tm " +
-		"    WHERE tm.team.id = p.team.id " +
-		"    AND tm.member.id = :memberId" +
-		"))")
-	Optional<Project> findByIdWithAccessCheck(@Param("projectId") Long projectId, @Param("memberId") Long memberId);
+    @Query("SELECT p FROM Project p " +
+        "WHERE p.id = :projectId " +
+        "AND p.deletedAt IS NULL " +
+        "AND p.team.deletedAt IS NULL " +
+        "AND (p.creator.id = :memberId " +
+        "OR EXISTS (" +
+        "    SELECT tm FROM TeamMember tm " +
+        "    WHERE tm.team.id = p.team.id " +
+        "    AND tm.member.id = :memberId " +
+        "    AND tm.deletedAt IS NULL" +
+        "))")
+    Optional<Project> findByIdWithAccessCheck(@Param("projectId") Long projectId, @Param("memberId") Long memberId);
+
+    Optional<Project> findByIdAndDeletedAtIsNull(Long id);
 }
